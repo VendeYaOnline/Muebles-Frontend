7ec9890c1f4b8953fc2c2efae253f71c6a6dc6f7
Yosip Mike Colin <vendeyaonline10@gmail.com>
2024-12-10 15:39:07 -0500

    Settings footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <h3 className="text-lg font-semibold mb-4">ElegantHogar</h3>
+            <h3 className="text-lg font-semibold mb-4">
+              Muebles y Electrodomésticos del Meta
+            </h3>
             <p className="text-gray-400">
               Transformando hogares con estilo desde 2023.
             </p>
@@ -59,7 +63,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div>
+          {/*           <div>
             <h4 className="text-lg font-semibold mb-4">Síguenos</h4>
             <div className="flex space-x-4">
               <a href="#" className="text-gray-400 hover:text-white">
@@ -104,12 +108,19 @@ const Footer = () => {
                 </svg>
               </a>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="mt-8 border-t border-gray-700 pt-8 text-center">
           <p className="text-gray-400">
-            &copy; 2023 ElegantHogar. Todos los derechos reservados.
+            &copy; 2023 Muebles y Electrodomésticos del Meta. Todos los derechos
+            reservados.
           </p>
+          <span className="text-xs">
+            Desarrollado por{" "}
+            <Link href="https://vendeyaonline.com/" target="_blank">
+              <strong>VendeYaOnline</strong>
+            </Link>
+          </span>
         </div>
       </div>
     </footer>
